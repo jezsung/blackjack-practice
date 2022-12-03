34478de68d3511423f3a0234eb290bf02e489d8d
Jaesung Hong <jezsung@gmail.com>
2022-12-03 17:05:25 -0600

    add tsc-files to type check only staged files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   // Type check
-  '**/*.(ts|tsx)': () => 'tsc --noEmit',
+  '**/*.(ts|tsx)': () => 'tsc-files --noEmit',
 
   // Lint
   '**/*.(ts|tsx|js)': (filenames) =>
