359af0b16af7ba0cdcd384e8fef76658c828b8c7
Jaesung Hong <jezsung@gmail.com>
2022-12-11 13:44:34 -0600

    add upcard getter to DealerHand

--- utils/dealer-hand.ts
+++ utils/dealer-hand.ts
@@ -9,6 +9,14 @@ class DealerHand extends Hand {
     super(cards);
   }
 
+  get upcard(): PlayingCard | null {
+    if (this.cards.length !== 2) {
+      return null;
+    }
+
+    return this.cards.find((card) => card.face === 'up')!;
+  }
+
   deal(...card: PlayingCard[]): void {
     this.cards.push(...card);
   }
