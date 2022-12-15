9fe3ed7e9876d5aebd457e0958d9a1aadd05454a
Jaesung Hong <jezsung@gmail.com>
2022-12-14 20:17:39 -0600

    add insurance asking popup

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,13 +12,13 @@ export default function Home() {
     dispatch(start());
   }, []);
 
-  const dealerCards = useAppSelector((state) => state.blackjack.dealerHand.cards);
+  const dealerHand = useAppSelector((state) => state.blackjack.dealerHand);
   const playerHands = useAppSelector((state) => state.blackjack.playerHands);
 
   return (
     <div className={classNames('relative h-screen')}>
       <div className={classNames('absolute top-8 left-1/2 -translate-x-1/2 flex gap-1')}>
-        {dealerCards.map((card) => (
+        {dealerHand.cards.map((card) => (
           <PlayingCardView key={`${card.rank}${card.suit}`} card={card} />
         ))}
       </div>
@@ -31,6 +31,17 @@ export default function Home() {
           </div>
         ))}
       </div>
+      {dealerHand.upcard?.rank === 'ace' && (
+        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 bg-white p-4 rounded">
+          <div className="flex flex-col justify-center items-center">
+            <div className="mb-8 text-xl">Insurance?</div>
+            <div className="flex gap-4">
+              <button className="bg-gray-400 min-w-[88px] w-36 h-12 rounded text-white">No</button>
+              <button className="bg-[#005bdc] min-w-[88px] w-36 h-12 rounded text-white">Yes</button>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex gap-4">
         <button className="bg-[#005bdc] min-w-[88px] w-36 h-12 rounded text-white">Hit</button>
         <button className="bg-[#005bdc] min-w-[88px] w-36 h-12 rounded text-white">Stand</button>
