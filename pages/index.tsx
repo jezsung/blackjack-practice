586861a178df0bbb3abbecf84983e04f37b75525
Jaesung Hong <jezsung@gmail.com>
2022-12-17 17:07:38 -0600

    add manual start button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import classNames from 'classnames';
-import { useEffect } from 'react';
 import PlayingCardView from '../components/PlayingCardView';
 import { useAppDispatch } from '../hooks/use-app-dispatch';
 import { useAppSelector } from '../hooks/use-app-selector';
@@ -7,10 +6,6 @@ import { bet, start } from '../store/slices/blackjack';
 
 export default function Home() {
   const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(start());
-  }, []);
 
   const dealerCards = useAppSelector((state) => state.blackjack.dealerHand.cards);
   const playerHands = useAppSelector((state) => state.blackjack.playerHands);
@@ -75,6 +70,9 @@ export default function Home() {
             onClick={() => dispatch(bet(100))}
           >
             $100
+          </button>
+          <button className="bg-[#005bdc] min-w-[88px] w-36 h-12 rounded text-white" onClick={() => dispatch(start())}>
+            Start
           </button>
         </div>
       </div>
