2ef060854bff30e64c65ddb53188f75657e1606e
Jaesung Hong <jezsung@gmail.com>
2023-01-01 12:16:01 -0600

    add dealer card value selector

--- store/slices/blackjack.ts
+++ store/slices/blackjack.ts
@@ -304,6 +304,51 @@ export const selectBet = (state: RootState): number => state.blackjack.betAmount
 
 export const selectInsured = (state: RootState): boolean => state.blackjack.insured;
 
+export const selectDealerCardValue = (state: RootState) => {
+  const sum = state.blackjack.dealerHand.cards.reduce((sum, card) => {
+    let value: number;
+
+    switch (card.rank) {
+      case '2':
+        value = 2;
+        break;
+      case '3':
+        value = 3;
+        break;
+      case '4':
+        value = 4;
+        break;
+      case '5':
+        value = 5;
+        break;
+      case '6':
+        value = 6;
+        break;
+      case '7':
+        value = 7;
+        break;
+      case '8':
+        value = 8;
+        break;
+      case '9':
+        value = 9;
+      case '10':
+      case 'jack':
+      case 'queen':
+      case 'king':
+        value = 10;
+        break;
+      case 'ace':
+        value = sum > 10 ? 1 : 11;
+        break;
+    }
+
+    return sum + value;
+  }, 0);
+
+  return sum;
+};
+
 export const selectPlayerCardValue = (state: RootState, handIndex: number): number => {
   const sum = state.blackjack.playerHands[handIndex].cards.reduce((sum, card) => {
     let value: number;
